3067ded2af25a3425960408c3fd064bd23424dee
vlastroia <vlad.stroia@gmail.com>
2019-06-21 14:34:48 +0300

    am limitat nr de litere la 8

--- pic.py
+++ pic.py
@@ -1,14 +1,17 @@
 import itertools
 litere = input("Care sunt literele?")
-nr_lit = int(input("Cate litere?"))
-file_name ="cuv/" + str(nr_lit) + "lit.txt"
-with open(file_name, "r") as f:
-    contents = f.readlines()        
-cuvinte = list(itertools.permutations(litere, nr_lit))
+if len(litere) <= 8:
+    nr_lit = int(input("Cate litere?"))
+    file_name ="cuv/" + str(nr_lit) + "lit.txt"
+    with open(file_name, "r") as f:
+        contents = f.readlines()        
+    cuvinte = list(itertools.permutations(litere, nr_lit))
 
-cuvinte = list(dict.fromkeys(cuvinte))
+    cuvinte = list(dict.fromkeys(cuvinte))
 
-for i in cuvinte:
-    i = "".join(i) + "\n"
-    if i in contents:
-        print(i)
+    for i in cuvinte:
+        i = "".join(i) + "\n"
+        if i in contents:
+            print(i)
+else:
+    print("prea multe litere")
